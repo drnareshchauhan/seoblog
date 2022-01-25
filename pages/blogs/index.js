40552a9d3161409ed7d417a155a1cfb0073dcd28
drnareshchauhan <drnareshchauhan@gmail.com>
2022-01-25 16:54:42 +0530

    new3

--- pages/blogs/index.js
+++ pages/blogs/index.js
@@ -128,7 +128,7 @@ const Blogs = ({
             <header>
               <div className="col-md-12 pt-3">
                 <h1 className="display-4 font-weight-bold text-center">
-                  Blogs from a weeb
+                  Blogs from Naresh
                 </h1>
               </div>
               <section>

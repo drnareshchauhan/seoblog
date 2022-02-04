bfdd44a7106cff8a6a762b81ea0884dd089d4d46
drnareshchauhan <drnareshchauhan@gmail.com>
2022-02-04 21:37:03 +0530

    favicon

--- pages/_app.js
+++ pages/_app.js
@@ -18,6 +18,7 @@ function MyApp({ Component, pageProps, router }) {
     <>
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <link rel="shortcut icon" href="/favicon.ico" />
       </Head>
       <Component {...pageProps} />
     </>

013ddabbb0a0c4808056ee76395f89ad141916dd
drnareshchauhan <drnareshchauhan@gmail.com>
2022-02-04 21:57:58 +0530

    new icons

--- pages/_app.js
+++ pages/_app.js
@@ -18,24 +18,24 @@ function MyApp({ Component, pageProps, router }) {
     <>
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <link rel="shortcut icon" href="/favicon.ico" />
+        <link rel="shortcut icon" href="/images/favicon.ico" />
         <link
           rel="icon"
           type="image/png"
           sizes="16x16"
-          href="/favicon-16x16.png"
+          href="/images/favicon-16x16.png"
         />
         <link
           rel="icon"
           type="image/png"
           sizes="32x32"
-          href="/favicon-32x32.png"
+          href="/images/favicon-32x32.png"
         />
         <link
           rel="icon"
           type="image/png"
           sizes="96x96"
-          href="/favicon-96x96.png"
+          href="/images/favicon-96x96.png"
         />
       </Head>
       <Component {...pageProps} />

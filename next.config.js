8ae1a84d1ab825928532ebbbd4b138b4421d9b10
drnareshchauhan <drnareshchauhan@gmail.com>
2022-02-26 23:13:44 +0530

    vercel

--- next.config.js
+++ next.config.js
@@ -5,7 +5,7 @@ module.exports = {
     API_PRODUCTION: "https://seoblogapi.herokuapp.com/api",
     PRODUCTION: true,
     DOMAIN_DEVELOPMENT: "http://localhost:3000",
-    DOMAIN_PRODUCTION: "https://www.drnareshchauhan.in",
+    DOMAIN_PRODUCTION: "https://drnareshchauhan.in",
     GOOGLE_CLIENT_ID:
       "59336272273-bvhr50g0f0monvrlqsn9mcdasbvbt47a.apps.googleusercontent.com",
     DISQUS_SHORTNAME: "naresh-5",
